1ad7c8eb9b85a978846f8bdec7253be85a5da864
NothingToSayInHere <adrianus_20@yahoo.com>
2022-12-01 17:06:43 +0100

    listview

--- app/src/main/java/com/example/noteapp3/MainActivity.java
+++ app/src/main/java/com/example/noteapp3/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.noteapp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
+        items = new ArrayList<>();
+        items.add("Apples");
+        items.add("Banana");
+        items.add("Orange");
+        items.add("Strawberry");
+        items.add("Kiwi");
+        items.add("Mango");
+
     }
 }
