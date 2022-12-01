ad337b682baa2be081ef2d77d901d7ff34e53f4b
NothingToSayInHere <adrianus_20@yahoo.com>
2022-12-01 17:42:59 +0100

    add setOnClickListener

--- app/src/main/java/com/example/noteapp3/MainActivity.java
+++ app/src/main/java/com/example/noteapp3/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> items;
     ArrayAdapter<String> adapter;
 
+    EditText input;
+    ImageView enter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
+
+        input = findViewById(R.id.input);
+        enter = findViewById(R.id.add);
+
         items = new ArrayList<>();
         items.add("Apples");
         items.add("Banana");
@@ -52,6 +61,21 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(adapter);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = input.getText().toString();//get text from input
+                if (!text.isEmpty()) { //if text is not empty
+                    items.add(text); //add text to items
+                    adapter.notifyDataSetChanged(); //refresh list
+                    input.setText("");// clear input
+                    Toast.makeText(MainActivity.this, "Added: " + text, Toast.LENGTH_SHORT).show();
+                } else { //if text is empty
+                    Toast.makeText(MainActivity.this, "Enter some text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
